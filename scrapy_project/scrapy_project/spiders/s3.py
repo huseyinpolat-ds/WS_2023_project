38297b21c342929503a058ef57f79f8495367e2e
Huseyin Polat <98704524+huseyinpolat-ds@users.noreply.github.com>
2023-06-06 01:44:13 +0200

    Update s3.py

--- scrapy_project/scrapy_project/spiders/s3.py
+++ scrapy_project/scrapy_project/spiders/s3.py
@@ -7,10 +7,6 @@ script1 = """
         function main(splash, args)
             assert(splash:go(args.url))
             assert(splash:wait(args.wait))
-            while not splash:select('div.RatingStatistics__column') do
-                print('waiting')
-                assert(splash:go(args.url))
-            end
             return {html = splash:html(),
                     url = splash:url()}
         end
@@ -52,7 +48,7 @@ class LinksSpider(scrapy.Spider):
     try:
         with open("book_links.csv", "rt") as f:
             if bool_param:
-                start_urls = [url.strip() for url in f.readlines()][1:11]
+                start_urls = [url.strip() for url in f.readlines()][1:101]
             else:
                 start_urls = [url.strip() for url in f.readlines()][1:]
     except:
@@ -60,28 +56,32 @@ class LinksSpider(scrapy.Spider):
   
     def start_requests(self):
         for url in self.start_urls:
-            yield SplashRequest(url, callback=self.parse,endpoint='execute',args={'lua_source': script1,'wait': 5, "url" : url, "timeout": 3000, "html" : 1})
+            yield SplashRequest(url, callback=self.parse,endpoint='execute',args={'lua_source': script1,'wait': 0.5, "url" : url, "timeout": 3000, "html" : 1})
  
     def parse(self, response):
+        if not response.xpath("//h1[@data-testid = 'bookTitle']/text()").get():
+            print("response not valid, making another request")
+            yield SplashRequest(response.url, callback=self.parse,dont_filter=True,endpoint='execute',args={'lua_source': script1,'wait': 0.5, "url" : response.url, "timeout": 3000, "html" : 1})
+        else:
+            p = Book()
+            title_element = ("//h1[@data-testid = 'bookTitle']/text()")
+            author_name_element = ("//span[contains(@class, 'ContributorLink__name')]/text()")
+            author_link_element = ("//a[contains(@class, 'ContributorLink')]/@href")
+            average_rating_element = ("//div[@class='BookPageMetadataSection__ratingStats']//div[@class='RatingStatistics__rating']/text()")
+            kindle_price_element = ("//span[contains(@class, 'Button__labelItem') and contains(text(), 'Kindle')]/text()")
+            rating_count_element = ("//div[@class='BookPageMetadataSection__ratingStats']//span[@data-testid='ratingsCount']/text()")
+            review_count_element = ("//div[@class='BookPageMetadataSection__ratingStats']//span[@data-testid='reviewsCount']/text()")
+            pages_element = ("//p[@data-testid='pagesFormat']/text()")
+            
+            p['link'] = response.url
+            p['title'] = response.xpath(title_element).get()    
+            p['author_name'] = response.xpath(author_name_element).get()
+            p['author_link'] = response.xpath(author_link_element).get()
+            p['average_rating'] = response.xpath(average_rating_element).get()
+            p['kindle_price'] = response.xpath(kindle_price_element).get()
+            p['rating_count'] = response.xpath(rating_count_element).get()
+            p['review_count'] = response.xpath(review_count_element).get()
+            p['n_pages'] = response.xpath(pages_element).get()
 
-        p = Book()
-        title_element = ("//h1[@data-testid = 'bookTitle']/text()")
-        author_name_element = ("//span[contains(@class, 'ContributorLink__name')]/text()")
-        author_link_element = ("//a[contains(@class, 'ContributorLink')]/@href")
-        average_rating_element = ("//div[@class='BookPageMetadataSection__ratingStats']//div[@class='RatingStatistics__rating']/text()")
-        kindle_price_element = ("//span[contains(@class, 'Button__labelItem') and contains(text(), 'Kindle')]/text()")
-        rating_count_element = ("//div[@class='BookPageMetadataSection__ratingStats']//span[@data-testid='ratingsCount']/text()")
-        review_count_element = ("//div[@class='BookPageMetadataSection__ratingStats']//span[@data-testid='reviewsCount']/text()")
-        pages_element = ("//p[@data-testid='pagesFormat']/text()")
+            yield p
         
-        p['link'] = response.url
-        p['title'] = response.xpath(title_element).get()    
-        p['author_name'] = response.xpath(author_name_element).get()
-        p['author_link'] = response.xpath(author_link_element).get()
-        p['average_rating'] = response.xpath(average_rating_element).get()
-        p['kindle_price'] = response.xpath(kindle_price_element).get()
-        p['rating_count'] = response.xpath(rating_count_element).get()
-        p['review_count'] = response.xpath(review_count_element).get()
-        p['n_pages'] = response.xpath(pages_element).get()
-
-        yield p
