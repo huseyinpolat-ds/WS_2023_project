c9c8304258e237782c4e5a9e198368ac4fd97880
Huseyin Polat <98704524+huseyinpolat-ds@users.noreply.github.com>
2023-06-05 19:21:38 +0200

    Update s3.py
    bool_param added

--- scrapy_project/scrapy_project/spiders/s3.py
+++ scrapy_project/scrapy_project/spiders/s3.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
+bool_param = True # When set to True, only first 100 pages will be scraped
 import scrapy
 from scrapy_splash import SplashRequest 
-from lxml import etree
-import json
 
 script1 = """
         function main(splash, args)
@@ -10,9 +9,8 @@ script1 = """
             assert(splash:wait(args.wait))
             while not splash:select('div.RatingStatistics__column') do
                 print('waiting')
-				assert(splash:go(args.url))
-				splash:wait(2)
-			end
+                assert(splash:go(args.url))
+            end
             return {html = splash:html()}
         end
             """
@@ -28,12 +26,10 @@ class Book(scrapy.Item):
     rating_count = scrapy.Field()
     review_count = scrapy.Field()
     n_pages = scrapy.Field()
-    publication_info = scrapy.Field()
 
 class LinksSpider(scrapy.Spider):
     name = 'book_details'
-    
-    
+
     custom_settings = {
     "SPLASH_URL" : "http://192.168.99.101:8050",
 
@@ -51,50 +47,40 @@ class LinksSpider(scrapy.Spider):
 
     "HTTPCACHE_STORAGE" : 'scrapy_splash.SplashAwareFSCacheStorage'}
     
-    
-    
     allowed_domains = ['www.goodreads.com']
     try:
         with open("book_links.csv", "rt") as f:
-            start_urls = [url.strip() for url in f.readlines()][1:]
+            if bool_param:
+                start_urls = [url.strip() for url in f.readlines()][1:101]
+            else:
+                start_urls = [url.strip() for url in f.readlines()][1:]
     except:
         start_urls = []
-    
-
-           
+  
     def start_requests(self):
         for url in self.start_urls:
-            yield SplashRequest(url, callback=self.parse,endpoint='execute',args={'lua_source': script1,'wait': 5, "url" : url, "timeout": 3000})
-            
-            
-           
+            yield SplashRequest(url, callback=self.parse,endpoint='execute',args={'lua_source': script1,'wait': 5, "url" : url, "timeout": 3000, "html" : 1})
+ 
     def parse(self, response):
-        # parser = etree.HTMLParser()
-        # tree = etree.fromstring(response.body, parser)
+
         p = Book()
         title_element = ("//h1[@data-testid = 'bookTitle']/text()")
         author_name_element = ("//span[contains(@class, 'ContributorLink__name')]/text()")
         author_link_element = ("//a[contains(@class, 'ContributorLink')]/@href")
         average_rating_element = ("//div[@class='BookPageMetadataSection__ratingStats']//div[@class='RatingStatistics__rating']/text()")
+        kindle_price_element = ("//span[contains(@class, 'Button__labelItem') and contains(text(), 'Kindle')]/text()")
         rating_count_element = ("//div[@class='BookPageMetadataSection__ratingStats']//span[@data-testid='ratingsCount']/text()")
         review_count_element = ("//div[@class='BookPageMetadataSection__ratingStats']//span[@data-testid='reviewsCount']/text()")
         pages_element = ("//p[@data-testid='pagesFormat']/text()")
         
-
-        if response.xpath(title_element).get():
-            p['link'] = response.request.url
-            p['title'] = response.xpath(title_element).get()    
-            p['author_name'] = response.xpath(author_name_element).get()
-            p['author_link'] = response.xpath(author_link_element).get()
-            p['average_rating'] = response.xpath(average_rating_element).get()
-            p['rating_count'] = response.xpath(rating_count_element).get()
-            p['review_count'] = response.xpath(review_count_element).get()
-            p['n_pages'] = response.xpath(pages_element).get()
-        else:
-            text = response.xpath('//body/script/text()').getall()
-            script_piece = response.xpath('//script[contains(text(),"author")]/text()').get()
-            p['link'] = 'json'
-            p['title'] = text
-            p['author_name'] = script_piece
+        p['link'] = response.request.url
+        p['title'] = response.xpath(title_element).get()    
+        p['author_name'] = response.xpath(author_name_element).get()
+        p['author_link'] = response.xpath(author_link_element).get()
+        p['average_rating'] = response.xpath(average_rating_element).get()
+        p['kindle_price'] = response.xpath(kindle_price_element).get()
+        p['rating_count'] = response.xpath(rating_count_element).get()
+        p['review_count'] = response.xpath(review_count_element).get()
+        p['n_pages'] = response.xpath(pages_element).get()
 
         yield p
